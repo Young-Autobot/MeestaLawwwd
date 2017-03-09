30e8c5ce16451cb21589ee074b841b2382414454
Young-Autobot <mikaredthunderhawk@gmail.com>
2017-03-09 23:40:14 +0000

    Added BLAST.

--- APA.py
+++ APA.py
@@ -1,9 +1,16 @@
 # Advanced Programming Assessment is Below
 # --------------------------------------------------------------------
+# All necessary libraries were imported.
 # SeqIO was imported in order to begin file conversion.
 from Bio import SeqIO
 # Groupby was imported from itertools for class FastaFile.
 from itertools import groupby
+# unittest was imported to for class Testing.
+import unittest
+# Both NCBIXML, and NCBIWWW were used for BLAST.
+from Bio.Blast import NCBIXML
+from Bio.Blast import NCBIWWW
+
 
 # A class created to parse FASTA file in a dictionary format.
 # class used to ask user to input value
@@ -13,11 +20,11 @@ class FastaFile(object):
         self.path = path
         # Map was used to help return a list of results.
         self._map = {}
-        self.fastaiter()
+        self.__fasta_iter()
 
-    # 'fastaiter' was called as a function.
-    # 'fastaiter' was used to help separate headings of the file.
-    def fastaiter(self):
+    # '__fasta_iter' was called as a function.
+    # '__fasta_iter' was used to help separate headings of the file.
+    def __fasta_iter(self):
         fasta = open(self.path)
         # The variable 'fasta_iter' created the keys starting at '>'.
         fasta_iter = (x[1] for x in groupby
@@ -47,7 +54,6 @@ class FastaFile(object):
     #Function created to look up input (ID) in the dictionary.
     #It then prints the sequence for associated ID.
     def user_input(self):
-
         #Loop was made and looped until told to stop.
         while True:
             # Command variable asked user for an input.
@@ -80,9 +86,9 @@ class FastaFile(object):
                 print "%s: %s" % (command, value)
 
 
-# GenBank file was downloaded and set as variable 'genbank_file'.
+# A GenBank file was downloaded and set as variable 'genbank_file'.
 genbank_file = ("/home/mika/AdvancedProgramming/GenBank")
-# An empty FASTA file was created and set as  variable 'fasta_file'.
+# An empty FASTA file was created and set as variable 'fasta_file'.
 fasta_file = ("/home/mika/AdvancedProgramming/Obs.fasta")
 
 # 'input' was created for when the GenBank file opened and read.
@@ -118,3 +124,30 @@ for sequence_id, sequence in cff:
 
 # Function within class was called using print statement.
 print cff.user_input()
+
+# cff was saved
+result = NCBIWWW.qblast("blastn", "nt", cff)
+
+#An XML file was created and saved.
+save = open("/home/mika/AdvancedProgramming/BLAST.xml", "w")
+save.write(result.read())
+save.close()
+result.close()
+
+#Opening the XML file was saved as a variable.
+result = open("/home/mika/AdvancedProgramming/BLAST.xml")
+
+#The output was parsed
+blast = NCBIXML.read(result)
+
+#Loops created an output of blast records with e value less than 0.01.
+for alignment in blast.alignments:
+    for hsp in alignment.hsps:
+        if hsp.expect < 0.01:
+            print('****Alignment****')
+            print('sequence:', alignment.title)
+            print('length:', alignment.length)
+            print('e value:', hsp.expect)
+            print(hsp.query[0:75] + '...')
+            print(hsp.match[0:75] + '...')
+            print(hsp.sbjct[0:75] + '...')
---
+++ test_fastaFile.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class TestFastaFile(TestCase):
+    def test___init__(self):
+        self.fail()
+
+
+class TestFastaFile(TestCase):
+    def test___fasta_iter(self):
+        self.fail()
+
+
+class TestFastaFile(TestCase):
+    def test_user_input(self):
+        self.fail()
